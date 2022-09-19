3c28cc61c909d666643f3caac0970c85a77d9430
Raph <raphael.lebailly@hotmail.com>
2022-09-19 18:42:56 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const { engine } = require ('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// set Handlebars Middleware
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+const anything = "anything";
+
+// set Handlebars routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        stuff:`This is ${anything} I want...`
+    });
+})
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
